06771085437dd5d39165e445c6f1a4ba5942b599
Andre Sch <andresch.dev@gmail.com>
2024-07-15 19:43:15 -0300

    Add cache operations

---
+++ core/src/main/java/com/reservas_cinema/domain/Cache.java
@@ -0,0 +1,59 @@
+package com.reservas_cinema.domain;
+
+import java.util.*;
+
+public class Cache {
+  private final Queue<Line> data = new LinkedList<>();
+  private final MainMemory memory;
+  private final int maxLines;
+
+  public Cache(MainMemory memory, int maxLines) {
+    this.memory = memory;
+    this.maxLines = maxLines;
+  }
+
+  public void writeBlock(int blockNumber, List<Long> block) {
+    var storedBlock = readBlock(blockNumber);
+
+    storedBlock.clear();
+    storedBlock.addAll(block);
+  }
+
+  public List<Long> readBlock(int blockNumber) {
+    for (var line : data) {
+      var blockIsAvailable = line.tag().blockNumber() == blockNumber;
+      if (blockIsAvailable) return line.content();
+    }
+
+    return fetchBlock(blockNumber);
+  }
+
+  public List<Long> fetchBlock(int blockNumber) {
+    if (data.size() == maxLines) releaseLine();
+    
+    var tag = new Tag(blockNumber);
+    var content = memory.readBlock(blockNumber);
+    
+    data.offer(new Line(tag, content));
+    return content;
+  }
+  
+  private void releaseLine() {
+    var line = data.poll();
+
+    var block = line.content();
+    var blockNumber = line.tag().blockNumber();
+
+    memory.writeBlock(blockNumber, block);
+  }
+}
+
+record Line(
+  Tag tag,
+  List<Long> content
+) {}
+
+record Tag(
+  int blockNumber
+  // todo: include MESI protocol status
+) {}
