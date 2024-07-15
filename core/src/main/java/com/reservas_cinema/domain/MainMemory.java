d3b5801146b2c57f54c4518c5b620d2b74e2a858
Andre Sch <andresch.dev@gmail.com>
2024-07-15 16:50:43 -0300

    Add memory operations

--- core/src/main/java/com/reservas_cinema/domain/MainMemory.java
+++ core/src/main/java/com/reservas_cinema/domain/MainMemory.java
@@ -1,7 +1,33 @@
 package com.reservas_cinema.domain;
 
-import java.util.List;
+import java.util.*;
 
 public class MainMemory {
-  public List<Long> data;
+  private final List<Long> data;
+  private final int addressesPerBlock;
+
+  public MainMemory(int numberOfAddresses, int addressesPerBlock) {
+    this.data = new ArrayList<>(numberOfAddresses);
+    this.addressesPerBlock = addressesPerBlock;
+  }
+
+  public Long readItem(int address) { return data.get(address); }
+  public Long writeItem(int address, Long value) { return data.set(address, value); }
+
+  public List<Long> readBlock(int blockNumber) {
+    List<Long> block = new LinkedList<>();
+
+    for (int i = firstAddress(blockNumber); i <= lastAddress(blockNumber); i++)
+      block.add(data.get(i));
+
+    return block;
+  }
+
+  public void writeBlock(int blockNumber, List<Long> block) {
+    for (int i = firstAddress(blockNumber), j = 0; i <= lastAddress(blockNumber); i++, j++)
+      data.set(i, block.get(j));
+  }
+
+  private int firstAddress(int blockNumber) { return blockNumber * addressesPerBlock; }
+  private int lastAddress(int blockNumber) { return firstAddress(blockNumber) + addressesPerBlock - 1; }
 }
