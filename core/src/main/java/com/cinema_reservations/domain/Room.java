6a8da8ed09d5664f76a1a6e2e55e7d0d8da605ee
Andre Sch <andresch.dev@gmail.com>
2024-08-10 12:53:29 -0300

    Update room interface by decoupling serialization

--- core/src/main/java/com/cinema_reservations/domain/Room.java
+++ core/src/main/java/com/cinema_reservations/domain/Room.java
@@ -2,52 +2,33 @@ package com.cinema_reservations.domain;
 
 import java.util.List;
 import java.util.LinkedList;
+import lombok.EqualsAndHashCode;
 
-public record Room(
-  long id,
-  long numberOfRows,
-  long seatsPerRow,
-  List<Seat> seats
-) {
-  public Room(
-    long id,
-    long numberOfRows,
-    long seatsPerRow,
-    List<Seat> seats
-  ) {
+@EqualsAndHashCode
+public class Room {
+  private long id;
+  private long numberOfRows;
+  private long seatsPerRow;
+  private List<Seat> seats;
+
+  public Room(long id) { this(id, 3, 9); }
+  public Room(long id, long numberOfRows, long seatsPerRow) {
     this.id = id;
     this.numberOfRows = numberOfRows;
     this.seatsPerRow = seatsPerRow;
+
+    this.seats = new LinkedList<>();
+    for (int i = 0; i < numberOfRows * seatsPerRow; i++)
+      this.seats.add(new Seat(ReserveStatus.AVAILABLE));
+  }
+
+  public Room(long id, long numberOfRows, long seatsPerRow, List<Seat> seats) {
+    this(id, numberOfRows, seatsPerRow);
     this.seats = seats;
-
-    if (seats.size() != numberOfRows * seatsPerRow)
-      throw new RuntimeException("seat quantity mismatch");
   }
 
-  public static Room deserialize(List<Long> serialization) {
-    var serializationCopy = new LinkedList<>(serialization);
-    int firstItem = 0;
-
-    long id = serializationCopy.remove(firstItem);
-    long numberOfRows = serializationCopy.remove(firstItem);
-    long seatsPerRow = serializationCopy.remove(firstItem);
-    List<Seat> seats = new LinkedList<>();
-
-    for (long serializedSeat : serializationCopy)
-      seats.add(Seat.deserialize(serializedSeat));
-
-    return new Room(id, numberOfRows, seatsPerRow, seats);
-  }
-
-  public List<Long> serialize() {
-    List<Long> serialization = new LinkedList<>();
-
-    serialization.add(id);
-    serialization.add(numberOfRows);
-    serialization.add(seatsPerRow);
-
-    seats.forEach((seat) -> serialization.add(seat.serialize()));
-    
-    return serialization;
-  }
+  public long getId() { return id; }
+  public long getNumberOfRows() { return numberOfRows; }
+  public long getSeatsPerRow() { return seatsPerRow; }
+  public List<Seat> getSeats() { return seats; }
 }
