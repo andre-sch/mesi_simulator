4694938bf2664985f6e75ab7ad7e73d24f11c417
Andre Sch <andresch.dev@gmail.com>
2024-08-16 19:11:53 -0300

    Remove selection from reserve status

--- core/src/main/java/com/cinema_reservations/domain/ReserveStatus.java
+++ core/src/main/java/com/cinema_reservations/domain/ReserveStatus.java
@@ -1,7 +1,7 @@
 package com.cinema_reservations.domain;
 
 public enum ReserveStatus {
-  AVAILABLE(0), SELECTED(1), RESERVED(2);
+  AVAILABLE(0), RESERVED(1);
 
   private int code;
 
@@ -16,14 +16,13 @@ public enum ReserveStatus {
   public static ReserveStatus of(int code) {
     switch (code) {
       case 0: return ReserveStatus.AVAILABLE;
-      case 1: return ReserveStatus.SELECTED;
-      case 2: return ReserveStatus.RESERVED;
+      case 1: return ReserveStatus.RESERVED;
       default: throw new RuntimeException("invalid status");
     }
   }
 
   public static ReserveStatus random() {
-    int randomCode = (int) Math.floor(Math.random() * 3);
+    int randomCode = (int) Math.floor(Math.random() * 2);
     return ReserveStatus.of(randomCode);
   }
 }
